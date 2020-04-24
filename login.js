dbe07e9ad7c7a0ae81aa8d6519378e005d4ff781
Matt Rentaro <hsudoru@gmail.com>
2020-04-24 09:46:47 +0800

    Update login.js

--- login.js
+++ login.js
@@ -2,6 +2,6 @@ function trackFunction() {
 	var firstname = document.getElementById("fname").value;
 	var lastname = document.getElementById("lname").value;
 	if (firstname == "woqchan" && lastname == "sen") {
-		window.open('https://drive.google.com/uc?export=download&id=1TZeVY8YOhUzje0gBw28qYLk_mrqgrbFY', '_self');
+		window.open('https://drive.google.com/uc?export=download&id=1TZeVY8YOhUzje0gBw28qYLk_mrqgrbFY');
 	}
 }
