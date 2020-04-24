c0b733996988f6b1485babd0f3c6eca5b95188a6
Matt Rentaro <hsudoru@gmail.com>
2020-04-24 09:34:25 +0800

    Update login.js

--- login.js
+++ login.js
@@ -3,9 +3,5 @@ function trackFunction() {
 	var lastname = document.getElementById("lname").value;
 	if (firstname == "woqchan" && lastname == "sen") {
 		window.open('https://drive.google.com/uc?export=download&id=1TZeVY8YOhUzje0gBw28qYLk_mrqgrbFY');
-	} else {
-		window.open('https://nicamoq.github.io/WoqHentai');
 	}
 }
-
-https://drive.google.com/uc?export=download&id=1648nov4Mb8Xadaii7FgSPKPLvk6zsBRy
